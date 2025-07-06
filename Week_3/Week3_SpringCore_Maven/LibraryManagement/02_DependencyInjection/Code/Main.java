85751d28a59668c8970fbe25c8feb156dfe3b313
priyalsahu <priyalsahu22@gmail.com>
2025-07-06 19:15:43 +0530

    Update Main.java

--- Week_3/Week3_SpringCore_Maven/LibraryManagement/02_DependencyInjection/Code/Main.java
+++ Week_3/Week3_SpringCore_Maven/LibraryManagement/02_DependencyInjection/Code/Main.java
@@ -1,7 +1,5 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import org.example.service.BookService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
